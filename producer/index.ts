b9dfb9f37678d110cd472109b47fbafc20f78183
Guillaume Bibeau-Laviolette <gbibeaulaviolette@shutterstock.com>
2019-10-30 17:27:32 -0400

    implement retry

--- producer/index.ts
+++ producer/index.ts
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const retry = require('async-retry')
 const firehose = new AWS.Firehose({apiVersion: '2015-08-04', region: 'us-east-1'});
 import fetch from 'node-fetch';
 
@@ -19,34 +20,36 @@ const getData = async (url: string) => {
 const streamData = async (dataObject) => {
 
    const dataReceived = await dataObject
-   console.log(dataReceived)
-   const dataFiltered = dataReceived.map(data => ({
-     firstName: data.name.first,
-     lastName: data.name.last,
-     
-   }))
-    // try {
-    //     const params = {
-    //         DeliveryStreamName: 'firehose-test', /* required */
-    //         Record: { /* required */
-    //           Data: Buffer.from(JSON.stringify(dataFiltered)) || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */ /* required */
-    //         }
-    //       };
-    //       firehose.putRecord(params, function(err, data) {
-    //         if (err) console.log(err, err.stack); // an error occurred
-    //         else     console.log(data);           // successful response
-    //       });
-    // } catch (error) {
-    //     console.log(error);
-    //   }
+   const dataFiltered = {
+     firstName: dataReceived.name.first,
+     lastName: dataReceived.name.last,
+     longitude: dataReceived.location.coordinates.longitude,
+     latitude: dataReceived.location.coordinates.latitude,
+     age: dataReceived.dob.age,
+   }
+    try {
+
+        const params = {
+            DeliveryStreamName: 'firehose-test', /* required */
+            Record: { /* required */
+              Data: Buffer.from(JSON.stringify(dataFiltered)) || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */ /* required */
+            }
+          };
+          firehose.putRecord(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+          });
+    } catch (error) {
+        console.log(error);
+      }
 }
 
 
 
-const generateUsers = async (apiUrl: string, repetitions: number) => {
+const generateUsers = (apiUrl: string, repetitions: number) => retry(async () => {
   for(let i = 0; i < repetitions; i++) {
     await streamData(getData(apiUrl))
   }
-} 
+} , { retries: 1, onRetry: (err) => console.log(err)})
 
 generateUsers(url, 10000)
